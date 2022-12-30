81757ae62bdf49c757129f17f83b1b9bea11f6b3
Gustavoefc <gusteric12@gmail.com>
2022-12-30 19:53:10 -0300

    Category

---
+++ src/main/java/com/initialCrud/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.initialCrud.course.repositories;
+
+import com.initialCrud.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+}
